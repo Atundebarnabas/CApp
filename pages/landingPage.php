39963d8499ff19a1473b96a6ba5ba695d118656b
BarnabasatundeJ <128942088+BarnabasatundeJ@users.noreply.github.com>
2024-05-16 21:52:47 +0100

    New update

--- pages/landingPage.php
+++ pages/landingPage.php
@@ -8,10 +8,83 @@
 </head>
 <body>
   <!-- Testing theme color -->
-  <div class="testtheme">1</div>
-  <div class="testtheme">2</div>
-  <div class="testtheme">3</div>
-  <div class="testtheme">4</div>
-  <div class="testtheme">5</div>
+  <header>
+    <div class="left">
+      <div class="icon webApp_icon"></div>
+    </div>
+    <div class="right">
+      <div class="menu desktopmenu">
+        <ul>
+          <div class="first">
+            <li class="active">Lorem</li>
+            <li>Lorem</li>
+            <li>Lorem</li>
+            <li>Lorem</li>
+            <li>Lorem</li>
+          </div>
+          <div class="last">
+            <li>lorem</li>
+            <li class="update">lorem</li>
+          </div>
+        </ul>
+      </div>
+      <div class="menu mobile_menu icons">
+        <div class="search icon">
+          <div class="svg_icon">
+            <!-- Title: “Search Alt SVG Vector”
+              Author: “Dazzle UI“— https://www.svgrepo.com/author/Dazzle%20UI/
+              Source: “svgrepo.com“— https://www.svgrepo.com/
+              License: “CC Attribution License”— https://www.svgrepo.com/page/licensing#CC%20Attribution 
+            -->
+												
+            <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offest="0%" style="stop-color: rgb(242, 112, 156);stop-opacity: 1"></stop>
+                <stop offest="100%" style="stop-color: rgb(255, 148, 114);stop-opacity: 1"></stop>
+              </linearGradient>
+            </defs>
+            <path d="M16.6725 16.6412L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+        </div>
+        <div class="hambuger_menu mobilemenu icon">
+          <div class="svg_icon">
+            <!-- Title: “Menu Hambuger SVG Vector”
+              Author: “basicons“— https://www.svgrepo.com/author/basicons/
+              Source: “svgrepo.com“— https://www.svgrepo.com/
+              License: “MIT License”— https://www.svgrepo.com/page/licensing/#MIT
+            -->
+            <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <line x1="5" y1="7" x2="19" y2="7" stroke-linecap="round" stroke-linejoin="round"/>
+            <line x1="5" y1="12" x2="19" y2="12" stroke-linecap="round" stroke-linejoin="round"/>
+            <line x1="5" y1="17" x2="19" y2="17" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <main>
+    <section id="one">
+      <div class="txts">
+        <div class="main_txt txt">
+          <p class="exp"  style="--delay: 0.4s;">Unleash</p> <p style="--delay: 0.8s;">Creativity:</p> <p style="--delay: 1s;">Explore •</p> <p class="exp" style="--delay: 1.2s;"> Create </p> <p style="--delay: 1.4s;">• Innovate</p>
+        </div>
+        <div class="sub_txt txt">
+          <p style="--delay: 2s;">Empowering Creativity: Embark on Your Journey to Inspired Expression and Unleash Your Unique Potential with Our App Today.</p>
+        </div>
+      </div>
+      <div class="get_started">
+        <button style="--delay: 2.8s;" type="button">Get Started</button>
+      </div>
+    </section>
+    <section id="two">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus beatae repellat est possimus, vel officiis assumenda inventore deleniti modi corrupti cum dolor, mollitia libero nam quis ipsam, reiciendis aperiam quasi id accusamus fugiat reprehenderit facilis. Alias sequi voluptate quas exercitationem atque vel laboriosam optio quisquam, soluta molestiae? Eius corporis eveniet magni facilis labore excepturi nisi a ipsam vero. Qui exercitationem expedita rem suscipit consequatur, cupiditate beatae quos esse distinctio ut. Facere earum culpa delectus saepe non ex aliquid est ad, ullam provident inventore et in pariatur? Laudantium aliquid minus dolorem, quisquam cupiditate cum magnam doloribus officiis a perspiciatis possimus, vero, enim veniam? Incidunt veritatis aliquid fugiat non explicabo sequi earum odit quis, cupiditate odio assumenda minima quae nemo, hic, inventore voluptas aperiam enim. Nemo natus alias doloribus expedita maiores, facere, labore quod ipsam vero nulla eaque consequatur molestias culpa. Nam asperiores tenetur ducimus nobis iste similique rerum, repellendus sint. Fuga debitis culpa expedita ipsa magni id a ab dicta, nihil ut qui totam unde delectus atque. Quod, nemo. Molestias voluptatem nostrum ducimus aspernatur officia autem, pariatur mollitia nulla magni placeat dolorem necessitatibus porro ab delectus quas, enim sint id perspiciatis? Quae, dolore facere dignissimos laborum repellat reprehenderit rerum veritatis velit.
+    </section>
+  </main>
+  <footer></footer>
 </body>
 </html>
