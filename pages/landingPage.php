61cceca13075aed2ab1149678ac74ecaab3f5ce5
BarnabasatundeJ <128942088+BarnabasatundeJ@users.noreply.github.com>
2024-05-21 00:32:00 +0100

    New update 21 may 2024

--- pages/landingPage.php
+++ pages/landingPage.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome  | CApp</title>
+  <title>Welcome | CApp</title>
   <link rel="stylesheet" href="../assets/css/landingpage.css">
 </head>
+
 <body>
   <!-- Testing theme color -->
   <header>
@@ -20,11 +22,11 @@
             <li style="--delay: 1s;">Blog</li>
             <li style="--delay: 1.2s;">Contact</li>
             <li style="--delay: 1.3s;">About</li>
-            <li style="--delay: 1.5s;">Discover</li>
+            <li style="--delay: 1.5s;">Discover <span class="update-notification-ball"></span></li>
           </div>
           <div class="last">
             <li style="--delay: 1.7s;">Videos</li>
-            <li class="update" style="--delay: 2.0s;">Updates</li>
+            <li class="update" style="--delay: 2.0s;">Updates <span class="update-notification-ball"></span></li>
           </div>
         </ul>
       </div>
@@ -36,16 +38,16 @@
               Source: “svgrepo.com“— https://www.svgrepo.com/
               License: “CC Attribution License”— https://www.svgrepo.com/page/licensing#CC%20Attribution 
             -->
-												
+
             <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
-                <stop offest="0%" style="stop-color: rgb(242, 112, 156);stop-opacity: 1"></stop>
-                <stop offest="100%" style="stop-color: rgb(255, 148, 114);stop-opacity: 1"></stop>
-              </linearGradient>
-            </defs>
-            <path d="M16.6725 16.6412L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke-linecap="round" stroke-linejoin="round"/>
+              <defs>
+                <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
+                  <stop offest="0%" style="stop-color: rgb(242, 112, 156);stop-opacity: 1"></stop>
+                  <stop offest="100%" style="stop-color: rgb(255, 148, 114);stop-opacity: 1"></stop>
+                </linearGradient>
+              </defs>
+              <path d="M16.6725 16.6412L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
           </div>
         </div>
@@ -58,10 +60,11 @@
             -->
             <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="5" y1="7" x2="19" y2="7" stroke-linecap="round" stroke-linejoin="round"/>
-            <line x1="5" y1="12" x2="19" y2="12" stroke-linecap="round" stroke-linejoin="round"/>
-            <line x1="5" y1="17" x2="19" y2="17" stroke-linecap="round" stroke-linejoin="round"/>
+              <line x1="5" y1="7" x2="19" y2="7" stroke-linecap="round" stroke-linejoin="round" />
+              <line x1="5" y1="12" x2="19" y2="12" stroke-linecap="round" stroke-linejoin="round" />
+              <line x1="5" y1="17" x2="19" y2="17" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
+            <span class="update-notification-ball" style="right: 0;"></span>
           </div>
         </div>
       </div>
@@ -71,7 +74,11 @@
     <section id="one">
       <div class="txts">
         <div class="main_txt txt">
-          <p class="exp"  style="--delay: 0.9s;">Unleash</p> <p style="--delay: 1s;">Creativity</p><br><br> <p style="--delay: 1.3s;">Explore •</p> <p style="--delay: 1.5s;"> Create </p> <p style="--delay: 1.8s;">• Innovate</p>
+          <p class="exp" style="--delay: 0.9s;">Unleash</p>
+          <p style="--delay: 1s;">Creativity</p><br><br>
+          <p style="--delay: 1.3s;">Explore •</p>
+          <p style="--delay: 1.5s;"> Create </p>
+          <p style="--delay: 1.8s;">• Innovate</p>
         </div>
         <div class="sub_txt txt">
           <p style="--delay: 2.0s;">Empowering Creativity: Embark on Your Journey to Inspired Expression and Unleash Your Unique Potential with Our App Today.</p>
@@ -82,9 +89,190 @@
       </div>
     </section>
     <section id="two">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus beatae repellat est possimus, vel officiis assumenda inventore deleniti modi corrupti cum dolor, mollitia libero nam quis ipsam, reiciendis aperiam quasi id accusamus fugiat reprehenderit facilis. Alias sequi voluptate quas exercitationem atque vel laboriosam optio quisquam, soluta molestiae? Eius corporis eveniet magni facilis labore excepturi nisi a ipsam vero. Qui exercitationem expedita rem suscipit consequatur, cupiditate beatae quos esse distinctio ut. Facere earum culpa delectus saepe non ex aliquid est ad, ullam provident inventore et in pariatur? Laudantium aliquid minus dolorem, quisquam cupiditate cum magnam doloribus officiis a perspiciatis possimus, vero, enim veniam? Incidunt veritatis aliquid fugiat non explicabo sequi earum odit quis, cupiditate odio assumenda minima quae nemo, hic, inventore voluptas aperiam enim. Nemo natus alias doloribus expedita maiores, facere, labore quod ipsam vero nulla eaque consequatur molestias culpa. Nam asperiores tenetur ducimus nobis iste similique rerum, repellendus sint. Fuga debitis culpa expedita ipsa magni id a ab dicta, nihil ut qui totam unde delectus atque. Quod, nemo. Molestias voluptatem nostrum ducimus aspernatur officia autem, pariatur mollitia nulla magni placeat dolorem necessitatibus porro ab delectus quas, enim sint id perspiciatis? Quae, dolore facere dignissimos laborum repellat reprehenderit rerum veritatis velit.
+      <div class="head">
+        <div class="section_title_txt content-to-animate">
+          <p>Stay<span class="color-out bordered">ing creat</span>ive</p>
+        </div>
+        <div class="section_description_txt content-to-animate">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!</p>
+        </div>
+      </div>
+      <div class="body">
+        <div class="img_content grid_layout_columns">
+          <div class="overlay-effect">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 0.6s;">
+            <img src="../assets/images/landingBg/a.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 0.3s;">
+            <img src="../assets/images/landingBg/b.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 1s;">
+            <img src="../assets/images/landingBg/c.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/d.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 1s;">
+            <img src="../assets/images/landingBg/e.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 1s;">
+            <img src="../assets/images/landingBg/creative_1.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/creative_bg.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/creative_2.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/b.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/a.jpg" alt="">
+          </div>
+
+        </div>
+        <div class="txt_content express_idea">
+          <ul>
+            <li>
+              <div class="content-to-animate head_txt">
+                <p><span class="bordered color-out">Expl</span>ore</p>
+              </div>
+              <div class="body_txt content-to-animate">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, aperiam veniam! Repudiandae, odio voluptates. Ipsam tenetur explicabo, quas asperiores tempore harum excepturi voluptatibus! Sit, impedit? Sapiente rerum, non numquam itaque id esse, quam voluptas architecto atque, repudiandae mollitia exercitationem perferendis!</p>
+              </div>
+            </li>
+            <li>
+              <div class="content-to-animate head_txt">
+                <p>Cr<span class="color-out bordered">ea</span>te</p>
+              </div>
+              <div class="body_txt content-to-animate">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, aperiam veniam! Repudiandae, odio voluptates. Ipsam tenetur explicabo, quas <span class="color-out bordered">asperiores tempore harum excepturi voluptatibus!</span> Sit, impedit? Sapiente rerum, non numquam itaque id esse, quam voluptas architecto atque, repudiandae mollitia exercitationem perferendis!</p>
+              </div>
+            </li>
+            <li>
+              <div class="content-to-animate head_txt">
+                <p>Inno<span class="color-out bordered">vate</span></p>
+              </div>
+              <div class="body_txt content-to-animate">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, aperiam veniam! Repudiandae, odio voluptates. Ipsam tenetur explicabo, quas asperiores tempore harum excepturi voluptatibus! Sit, impedit? Sapiente rerum, non numquam itaque id esse, quam voluptas architecto atque, repudiandae mollitia exercitationem perferendis!</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="two">
+      <div class="head">
+        <div class="section_title_txt content-to-animate">
+          <p>Stay<span class="color-out bordered">ing creat</span>ive</p>
+        </div>
+        <div class="section_description_txt content-to-animate">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio!</p>
+        </div>
+      </div>
+      <div class="body">
+        <div class="img_content grid_layout_columns">
+          <div class="overlay-effect">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 0.6s;">
+            <img src="../assets/images/landingBg/a.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 0.3s;">
+            <img src="../assets/images/landingBg/b.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 1s;">
+            <img src="../assets/images/landingBg/c.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/d.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container" style="--delay: 1s;">
+            <img src="../assets/images/landingBg/e.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 1s;">
+            <img src="../assets/images/landingBg/creative_1.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/creative_bg.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/creative_2.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/b.jpg" alt="">
+          </div>
+          <div class="content-to-animate img_container not-mobile" style="--delay: 0.7s;">
+            <img src="../assets/images/landingBg/a.jpg" alt="">
+          </div>
+
+        </div>
+        <div class="txt_content express_idea">
+          <ul>
+            <li>
+              <div class="content-to-animate head_txt">
+                <p><span class="bordered color-out">Expl</span>ore</p>
+              </div>
+              <div class="body_txt content-to-animate">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, aperiam veniam! Repudiandae, odio voluptates. Ipsam tenetur explicabo, quas asperiores tempore harum excepturi voluptatibus! Sit, impedit? Sapiente rerum, non numquam itaque id esse, quam voluptas architecto atque, repudiandae mollitia exercitationem perferendis!</p>
+              </div>
+            </li>
+            <li>
+              <div class="content-to-animate head_txt">
+                <p>Cr<span class="color-out bordered">ea</span>te</p>
+              </div>
+              <div class="body_txt content-to-animate">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, aperiam veniam! Repudiandae, odio voluptates. Ipsam tenetur explicabo, quas <span class="color-out bordered">asperiores tempore harum excepturi voluptatibus!</span> Sit, impedit? Sapiente rerum, non numquam itaque id esse, quam voluptas architecto atque, repudiandae mollitia exercitationem perferendis!</p>
+              </div>
+            </li>
+            <li>
+              <div class="content-to-animate head_txt">
+                <p>Inno<span class="color-out bordered">vate</span></p>
+              </div>
+              <div class="body_txt content-to-animate">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, aperiam veniam! Repudiandae, odio voluptates. Ipsam tenetur explicabo, quas asperiores tempore harum excepturi voluptatibus! Sit, impedit? Sapiente rerum, non numquam itaque id esse, quam voluptas architecto atque, repudiandae mollitia exercitationem perferendis!</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
   </main>
   <footer></footer>
+
+  <script>
+    // Animate on scroll
+
+    // Observer
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+        } else {
+          entry.target.classList.remove('animate');
+        }
+      });
+    });
+
+    const contentToAnimate = document.querySelectorAll('.content-to-animate');
+    contentToAnimate.forEach((content) => observer.observe(content));
+
+    // Add the 'animate' to all '.content-to-animate' class
+    document.addEventListener('DOMContentLoaded', function() {
+      contentToAnimate.forEach(content => {
+        content.classList.add('animate');
+      });
+    });
+
+    setTimeout(function() {
+      contentToAnimate.forEach(content => {
+        content.classList.remove('animate');
+      });
+    }, 1000);
+  </script>
 </body>
+
 </html>
